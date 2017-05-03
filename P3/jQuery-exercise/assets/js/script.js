3239a5467ee3066a4367304c737c080b6af6910c
Jake Dessau <jdessau@risd.edu>
2017-05-03 00:21:34 -0400

    adding p3
    adding project 3

---
+++ P3/jQuery-exercise/assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	// your code here
+
+
+		
+	// n = 0;
+	// $('body').click(function(){
+	// 	n = n + 1;	
+	// 	$('#pass').css('opacity', n/20);
+	// });
+
+	// $('body').mousemove(function(e){
+	// 	$('#pass').css('opacity', + (e.pageY) / 10000);
+	// });
+
+});
