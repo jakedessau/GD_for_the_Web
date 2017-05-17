f499157b12d1cef283df97114e17e7fd4bfb75f7
Jake Dessau <jdessau@risd.edu>
2017-05-17 17:51:51 -0400

    updatedp2

--- P3/final/script.js
+++ P3/final/script.js
@@ -4,4 +4,12 @@ $(function() {
 	$( ".my-special-div" ).draggable();
 });
 
+$('.my-special-div').mouseenter(function() {
+     $('.blue-square').fadeOut();
+});
+
+$('.my-special-div').mouseleave(function() {
+     $('.blue-square').fadeIn();
+});
+
 });
